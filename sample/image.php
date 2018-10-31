3390f53be6d5326b9565b5e5cc595792421aca3a
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-31 15:03:20 +0700

    Update ImageResize

---
+++ sample/image.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project image.
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/31/18
+ * Time: 14:44
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+$url = 'http://sctv.tv247.vn/public/uploads/sctv/logo_nen5_18_1535073239.png';
+
+
+\nguyenanhung\MyImage\ImageResize::process($url);
+
+
--- src/ImageResize.php
+++ src/ImageResize.php
@@ -24,20 +24,63 @@ class ImageResize
      * Function process
      *
      * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 10/31/18 11:12
+     * @time  : 10/31/18 15:01
      *
-     * @param string $url Url Image or Path to Image
-     * @param int    $width
-     * @param int    $height
+     * @param string $url    Đường dẫn hoặc URL hình ảnh
+     * @param int    $width  Thiết lập thông số chiều rộng
+     * @param int    $height Thiết lập thông số chiều cao
+     * @param string $format Format đầu ra
      *
      * @return \Imagine\Image\ImageInterface
      */
-    public static function process($url = '', $width = 100, $height = 100)
+    public static function process($url = '', $width = 100, $height = 100, $format = 'png')
     {
         $imagine = new Imagine();
-        $image   = $imagine->open($url);
+        if (is_file($url)) {
+            $image = $imagine->open($url);
+        } else {
+            $url   = static::getImageFromUrl($url);
+            $image = $imagine->load($url);
+        }
         $image->resize(new Box($width, $height), ImageInterface::FILTER_UNDEFINED);
 
-        return $image->show('jpg');
+        return $image->show($format);
+    }
+
+    /**
+     * Function getImageFromUrl
+     *
+     * Sử dụng cURL để Get Content Image
+     *
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 10/31/18 14:43
+     *
+     * @param string $url
+     *
+     * @return mixed|null
+     */
+    public static function getImageFromUrl($url = '')
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "GET",
+            CURLOPT_HTTPHEADER     => [
+                "cache-control: no-cache"
+            ],
+        ]);
+        $response = curl_exec($curl);
+        $err      = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            return NULL;
+        } else {
+            return $response;
+        }
     }
 }
